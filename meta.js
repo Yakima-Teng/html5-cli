bc61bd61d7dda0123fa524d8311131bb2be2977c
Yakima <cleveryun@163.com>
2019-02-25 23:46:34 +0800

    update

--- meta.js
+++ meta.js
@@ -6,8 +6,6 @@ module.exports = {
     }
   },
   filters: {
-    'bin/**/*': 'false',
-    'bin-lib/**/*': 'false',
     'build/**/*': 'true',
     'lib/**/*': 'true',
     'src/**/*': 'shouldKeepExampleFiles',
@@ -22,7 +20,11 @@ module.exports = {
     'site.proxy.config.js': 'true'
   },
   skipInterpolation: 'src/**/*.bmp',
-  complete (data, { chalk }) {
-    console.log(chalk.green('success'))
+  complete (data, { chalk, logger, files, fs }) {
+    console.log(`data: ${JSON.stringify(data, null, 2)}`)
+    fs.mkdirSync('src')
+    fs.writeFile('src/index.ejs', '')
+    fs.writeFile('src/app.scss', '')
+    fs.writeFile('src/app.js', '')
   }
 }
