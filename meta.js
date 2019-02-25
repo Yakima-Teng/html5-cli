d44d53932d9fe8889f7d3a0a757bbcf2950b19f5
Yakima <cleveryun@163.com>
2019-02-26 00:09:36 +0800

    enable generating simple source files

--- meta.js
+++ meta.js
@@ -1,3 +1,30 @@
+const fileContentForIndexEJS = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="id=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+Just an example
+<script src="./app.js"></script>
+</body>
+</html>
+`
+
+const fileContentForAppScss = `
+body {}
+`
+
+const fileContentForAppJS = `
+// your code here
+`
+
 module.exports = {
   prompts: {
     'shouldKeepExampleFiles': {
@@ -21,10 +48,20 @@ module.exports = {
   },
   skipInterpolation: 'src/**/*.bmp',
   complete (data, { chalk, logger, files, fs }) {
-    console.log(`data: ${JSON.stringify(data, null, 2)}`)
-    fs.mkdirSync('src')
-    fs.writeFile('src/index.ejs', '')
-    fs.writeFile('src/app.scss', '')
-    fs.writeFile('src/app.js', '')
+    if (data.shouldKeepExampleFiles === true) {
+      const distDirName = data.distDirName
+      const inPLace = data.inPlace
+      if (inPLace === true) {
+        fs.mkdirSync('src')
+        fs.writeFileSync('src/index.ejs', fileContentForIndexEJS)
+        fs.writeFileSync('src/app.scss', fileContentForAppScss)
+        fs.writeFileSync('src/app.js', fileContentForAppJS)
+      } else {
+        fs.mkdirSync(`${distDirName}/src`)
+        fs.writeFileSync(`${distDirName}/src/index.ejs`, fileContentForIndexEJS)
+        fs.writeFileSync(`${distDirName}/src/app.scss`, fileContentForAppScss)
+        fs.writeFileSync(`${distDirName}/src/app.js`, fileContentForAppJS)
+      }
+    }
   }
 }
