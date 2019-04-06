c3391dd4e650c284b95967d195545a1b6aa6afb2
Yakima <cleveryun@163.com>
2019-04-06 15:00:34 +0800

    支持通过INLINE环境变量区分处理是否需要将css、js文件内联至html中的情况

--- template/build/tasks/ejs.js
+++ template/build/tasks/ejs.js
@@ -5,7 +5,14 @@ const htmlmin = require('gulp-htmlmin')
 const gulpif = require('gulp-if')
 const inlineSource = require('gulp-inline-source')
 
-const { isProduction, join, browserSync, projectName, getVersionDate } = require('../utils')
+const {
+    isProduction,
+    join,
+    browserSync,
+    projectName,
+    getVersionDate,
+    shouldInline,
+} = require('../utils')
 
 module.exports = () => {
     // 需要先删除require缓存的数据，否则renderData的数据是不会随着config.js中renderData的改变而改变的
@@ -14,21 +21,27 @@ module.exports = () => {
             delete require.cache[key]
         }
     })
+    const rawPipeLines = gulp.src([join('/src/**/*.ejs'), '!' + join('/src/**/*.no.ejs')])
+        .pipe(ejs((() => {
+            const renderData = require('../../src/site.data.config')
+            renderData.projectName = projectName
+            renderData.versionDate = getVersionDate()
+            return renderData
+        })(), {}, { ext: '.html' })).on('error', log)
+        .pipe(gulpif(isProduction, htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            removeAttributeQuotes: true,
+        })))
     try {
-        return gulp.src([join('/src/**/*.ejs'), '!' + join('/src/**/*.no.ejs')])
-            .pipe(ejs((() => {
-                const renderData = require('../../src/site.data.config')
-                renderData.projectName = projectName
-                renderData.versionDate = getVersionDate()
-                return renderData
-            })(), {}, { ext: '.html' })).on('error', log)
-            .pipe(gulpif(isProduction, htmlmin({
-                collapseWhitespace: true,
-                removeComments: true,
-                removeAttributeQuotes: true,
-            })))
-            .pipe(gulp.dest(join(`/${projectName}`)))
-            .pipe(inlineSource())
+        if (shouldInline) {
+            return rawPipeLines
+                .pipe(gulp.dest(join(`/${projectName}`)))
+                .pipe(inlineSource())
+                .pipe(gulp.dest(join(`/${projectName}`)))
+                .pipe(browserSync.stream())
+        }
+        return rawPipeLines
             .pipe(gulp.dest(join(`/${projectName}`)))
             .pipe(browserSync.stream())
     } catch (e) {
--- template/build/tasks/remove-redundant-folder.js
+++ template/build/tasks/remove-redundant-folder.js
@@ -1,9 +1,18 @@
+const gulp = require('gulp')
 const fse = require('fs-extra')
 
-const { join, projectName } = require('../utils')
+const {
+    join,
+    projectName,
+    shouldInline,
+} = require('../utils')
 
 module.exports = () => {
-    // 因为添加了inline属性的link/script标签所引用的js和css文件会被内联到html中，所以产物中不需要保留css和js目录
-    fse.remove(join(`/${projectName}/js`))
-    return fse.remove(join(`/${projectName}/css`))
+    if (shouldInline) {
+        // 因为添加了inline属性的link/script标签所引用的js和css文件会被内联到html中，所以产物中不需要保留css和js目录
+        fse.removeSync(join(`/${projectName}/js`))
+        return fse.remove(join(`/${projectName}/css`))
+    }
+    // 这里具体gulp.src的文件不重要，只是为了返回一个"空"的gulp任务
+    return gulp.src(join('/src/site.data.config.js'))
 }
